46cbd8f391ecc741f1d91b71c905bb31c39c5793
leila771 <awawouleila@gmail.com>
2024-07-05 19:20:30 +0100

    test

--- api/app.py
+++ api/app.py
@@ -1,33 +1,33 @@
 # app.py
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from transformers import MarianMTModel, MarianTokenizer
+# from transformers import MarianMTModel, MarianTokenizer
 
 
 app = Flask(__name__)
 CORS(app)
 
 # Load pre-trained model and tokenizer
-model_name = "jamm55/autotrain-improved-pidgin-model-2837583189"
-tokenizer = MarianTokenizer.from_pretrained(model_name)
-model = MarianMTModel.from_pretrained(model_name)
+# model_name = "jamm55/autotrain-improved-pidgin-model-2837583189"
+# tokenizer = MarianTokenizer.from_pretrained(model_name)
+# model = MarianMTModel.from_pretrained(model_name)
 
-@app.post('/translate')
-def translate_to_pidgin():
+# @app.post('/translate')
+# def translate_to_pidgin():
 
-    text = request.json.get('text')
+#     text = request.json.get('text')
     
     
-    # Tokenize input text
-    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
+#     # Tokenize input text
+#     inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
 
-    # Perform translation
-    outputs = model.generate(**inputs)
+#     # Perform translation
+#     outputs = model.generate(**inputs)
 
-    # Decode translated text
-    translated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+#     # Decode translated text
+#     translated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-    return jsonify(translated_text)
+#     return jsonify(translated_text)
 
 @app.get('/')
 def index():
